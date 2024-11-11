ffe59ad3f2e413dac81101af9a6a566341ba0984
TK <161320773+2letrinhas@users.noreply.github.com>
2024-11-11 08:15:31 -0300

    Add files via upload

---
+++ kaoz/Projeto-cards---final-main/perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Música',
+    'O albúm "Donda" foi lançado por qual artista?',
+    'Kanye West'
+)
+
+criaCartao(
+    'Música',
+    'O albúm "Utopia" foi lançado por qual artista?',
+    'Travis Scott'
+)
+
+criaCartao(
+    'Música',
+    'O artista "Kanye West" possui quantos Grammys?',
+    '24'
+)
+
+criaCartao(
+    'Música',
+    '"slim Shady" foi uma personalidade criada pelo artista...',
+    'Eminem'
+)
+
+criaCartao(
+    'Música',
+    'Em questão de números, quem é o maior rapper atualmente?',
+    'Drake'
+)
+criaCartao(
+    'Música',
+    'A música "Not Like Us" é de qual artista?',
+    'Kendrick Lamar'
+)
